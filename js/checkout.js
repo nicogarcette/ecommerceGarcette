f00b4ec1b875c469407f43d1150c87acf89b7c18
nicogarcette <nicolasgarcette@outlook.com>
2022-06-23 18:11:49 -0300

    validando submit

--- js/checkout.js
+++ js/checkout.js
@@ -15,75 +15,109 @@ const expresiones = {
 	phone: /^\d{8,14}$/ // 8 a 14 numeros.
 }
 
+// objeto para validar que todos los campos esten correctos.
+const campos={
+    nombre:false,
+    surname:false,
+    email:false,
+    phone:false,
+    dni:false
+}
+
+
+const checkCampos=()=>{return campos.nombre && campos.surname && campos.email && campos.dni && campos.phone};
+
+// Agregar un novalidateatributo al elemento del formulario evita la validación nativa en los elementos del formulario (si se aplica), 
+// lo que permite que JavaScript administre todas las validaciones sin obstrucciones.
+form.noValidate=true;
 form.addEventListener("submit", e=>{
     e.preventDefault();
-    
-  
 
+    inputs.forEach(input => {
+        validarFormulario(input);   
+    });
+    
+    if (checkCampos()) {
+        swal({
+            title: "Te estamos redirigiendo a Mercado Pago",
+            text: "¡Gracias por confiar!",
+            button: false
+        }).then(setTimeout(() => {
+            // mercadopago();
+        }, 3000));
+    }else{
+        console.log("datos incorrectos");
+    }
+
+    
 })
 
 
-const valueWrong=(nodo)=>{
-    const padre=nodo.parentElement.parentElement;
+const valueWrong=(input)=>{
+    const padre=input.parentElement.parentElement;
     padre.className="form_grupo-incorrecto";
+    if (input.value==="") {
+       const p=padre.querySelector("p");
+       p.innerText="Este campo es obligatorio.";
+    }
+
 }
-const valueRight=(nodo)=>{
-    const padre=nodo.parentElement.parentElement;
+
+const valueRight=(input,message)=>{
+    const padre=input.parentElement.parentElement;
     padre.className="form_grupo-correcto";
 
+}
+
+
+const validarCampo=(input,expresion)=>{
+
+    
+    if (input.value==="") {
+        valueWrong(input);
+        return false;
+    }else if (!expresion.test(input.value)) {
+        valueWrong(input,``);
+        return false;
+    }else{
+        valueRight(input);
+        return true;
+    }
+
+}
+const validarPhone=()=>{
+
+    if (phone.value==="") {
+        valueWrong(phone);
+        campos.phone=false;
+    }else if ((!expresiones.phone.test(phone.value))||(phone.value.length<10)) {
+        valueWrong(phone);
+        campos.phone=false;
+    }else{
+        valueRight(phone);
+        campos.phone=true;
+    }
 }
 
 const validarFormulario=(input)=>{
 
     switch (input) {
         case nombre:
-            if (nombre.value==="") {
-                valueWrong(nombre);
-            }else if (!expresiones.nombre.test(nombre.value)) {
-                valueWrong(nombre);
-            }else{
-                valueRight(nombre);
-            }
+            campos.nombre=validarCampo(nombre,expresiones.nombre,campos.nombre);
             break;
 
         case surname:
-            if (surname.value==="") {
-                valueWrong(surname);
-            }else if (!expresiones.nombre.test(surname.value)) {
-                valueWrong(surname);
-            }else{
-                valueRight(surname);
-            }
-        
+            campos.surname=validarCampo(surname,expresiones.nombre,campos.surname);
             break;
+
         case email:
-            if (email.value==="") {
-                valueWrong(email);
-            }else if (!expresiones.email.test(email.value)) {
-                valueWrong(email);
-            }else{
-                valueRight(email);
-            }
-        
+            campos.email=validarCampo(email,expresiones.email,campos.email);
             break;
         case phone:
-            console.log(phone.value.lenght)
-            if (phone.value==="") {
-                valueWrong(phone);
-            }else if ((!expresiones.phone.test(phone.value))||(phone.value.length<10)) {
-                valueWrong(phone);
-            }else{
-                valueRight(phone);
-            }
+            validarPhone();
             break;
         case dni:
-            if (dni.value==="") {
-                valueWrong(dni);
-            }else if ((!expresiones.phone.test(dni.value))) {
-                valueWrong(dni);
-            }else{
-                valueRight(dni);
-            }
+            campos.dni=validarCampo(dni,expresiones.phone,campos.dni);
             break;
    
         default:
@@ -96,3 +130,4 @@ inputs.forEach(input => {
     input.addEventListener(`keyup`,()=>validarFormulario(input));
     input.addEventListener(`blur`,()=>validarFormulario(input));    
 });
+
